d4696474104332c5058620a34eba7479b7f9646a
Kevin Old <kevin@kevinold.com>
2023-09-15 15:38:12 -0500

    get app env

--- update-env.sh
+++ update-env.sh
@@ -8,6 +8,9 @@ allowlist=(
   VPC_LAMBDA_FUNCTION_NAME
 )
 
+# Get the app environment from amplify/.config/local-env-info.json
+$APP_ENV=$(jq -r '.envName' amplify/.config/local-env-info.json)
+
 for key in "${allowlist[@]}"; do
   echo "$key=$(aws ssm get-parameter --name "/amplify/$AWS_APP_ID/$APP_ENV/nextAPI/$key" --with-decryption | jq '.Parameter.Value')" >> .env
 done
