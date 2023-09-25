c15070de669df84256700e2dca2f7c10370f0030
Kevin Old <kevin@kevinold.com>
2023-09-25 14:41:29 -0500

    update to use APP_BRANCH and AWS_APP_ID

--- sync-ssm-params.sh
+++ sync-ssm-params.sh
@@ -8,25 +8,17 @@ allowlist=(
   VPC_LAMBDA_FUNCTION_NAME
 )
 
-# Get the app environment from amplify/.config/local-env-info.json
-APP_ENV=$(jq -r '.envName' amplify/.config/local-env-info.json)
+
+# Get the name of the current branch
+APP_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Load .env into environment
 export $(cat .env.local | grep -v '^#' | xargs) 
-
-# Check if AMPLIFY_APP_ID already exists and if so, exit because we are running in CI/CD
-if [ ! -z "$AMPLIFY_APP_ID" ]; then
-  echo "AMPLIFY_APP_ID already set, exiting in case this is running in CI/CD..." 
-  exit 1
-fi
-
-# If we are not running in CI/CD, get AmplifyAppId from amplify/team-provider-info.json
-AMPLIFY_APP_ID=$(jq -r ".$APP_ENV.awscloudformation.AmplifyAppId" amplify/team-provider-info.json)
 
 # Get AWS access keys from AWS CLI profile
 AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile $AWS_PROFILE)
 AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile $AWS_PROFILE)
 
 for key in "${allowlist[@]}"; do
-  aws ssm put-parameter --name "/amplify/$AMPLIFY_APP_ID/$APP_ENV/nextAPI/$key" --value "${!key}" --type SecureString --overwrite
+  aws ssm put-parameter --name "/amplify/$AWS_APP_ID/$APP_BRANCH/nextAPI/$key" --value "${!key}" --type SecureString --overwrite
 done
--- update-env.sh
+++ update-env.sh
@@ -8,9 +8,9 @@ allowlist=(
   VPC_LAMBDA_FUNCTION_NAME
 )
 
-# Get the app environment from amplify/.config/local-env-info.json
-APP_ENV=$(jq -r '.envName' amplify/.config/local-env-info.json)
+# Get the name of the current branch
+APP_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 for key in "${allowlist[@]}"; do
-  echo "$key=$(aws ssm get-parameter --name "/amplify/$AWS_APP_ID/$APP_ENV/nextAPI/$key" --with-decryption | jq '.Parameter.Value')" >> .env
+  echo "$key=$(aws ssm get-parameter --name "/amplify/$AWS_APP_ID/$APP_BRANCH/nextAPI/$key" --with-decryption | jq '.Parameter.Value')" >> .env
 done
