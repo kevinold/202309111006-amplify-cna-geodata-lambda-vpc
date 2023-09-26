4bcc1d661faa8eb0936defd759f3cbf2681bcbaa
Kevin Old <kevin@kevinold.com>
2023-09-26 16:27:35 -0500

    update env with jq for secrets

--- update-env.sh
+++ update-env.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+echo $secrets | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' >> .env
+
 # Allow list of parameters
-allowlist=(
-  AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY
-  VPC_AWS_REGION
-  VPC_LAMBDA_FUNCTION_NAME
-)
+# allowlist=(
+#   AWS_ACCESS_KEY_ID
+#   AWS_SECRET_ACCESS_KEY
+#   VPC_AWS_REGION
+#   VPC_LAMBDA_FUNCTION_NAME
+# )
 
 # TEST_PARAM=$(aws ssm get-parameter --name "/amplify/d36g0ambyeydeu/main/test" --with-decryption | jq '.Parameter.Value')
 # echo $TEST_PARAM
 
 # Get the name of the current branch
-APP_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo $APP_BRANCH
+# APP_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# echo $APP_BRANCH
 
-for key in "${allowlist[@]}"; do
-  echo "$key=$(aws ssm get-parameter --name "/amplify/$AWS_APP_ID/$APP_BRANCH/$key" --with-decryption | jq '.Parameter.Value')" >> .env
-done
+# for key in "${allowlist[@]}"; do
+#   echo "$key=$(aws ssm get-parameter --name "/amplify/$AWS_APP_ID/$APP_BRANCH/$key" --with-decryption | jq '.Parameter.Value')" >> .env
+# done
